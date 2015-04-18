5c7f738c514ef03507e3e8ed183a3d6793be50d9
Anton <anton.yevtukh@gmail.com>
2015-04-19 01:05:32 +0300

    Invalid atribute fix

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,5 @@
 
 ActiveRecord::Schema.define(version: 20150418211043) do
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "string"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
