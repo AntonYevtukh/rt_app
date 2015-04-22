2743d3e7f89ad3ce7e12f8f89c7eaf5aedf6de35
Anton <anton.yevtukh@gmail.com>
2015-04-23 02:39:49 +0300

    Failed registration functional

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ VALID_PASSWORD_REGEX = /\A[\w+]/i
 validates :name, presence: true, length: { maximum: 64 }
 validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 uniqueness: { case_sensitive: false }
-validates :password, presence: true, length: { minimum: 8}, format: {with: VALID_PASSWORD_REGEX}
-validates :password_confirmation, presence: true, length: { minimum: 8}, format: {with: VALID_PASSWORD_REGEX}
+validates :password, length: { minimum: 8}, format: {with: VALID_PASSWORD_REGEX}
+validates :password_confirmation, presence: true
 
 end
